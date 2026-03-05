19a7914c20ca61e010a9fe517b2dc908cca38f4a
Junha <vuswnsgk724@gamil.com>
2026-03-05 16:15:54 +0900

    내용 추가

---
+++ TestProject/review.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const review = () => {
+  return (
+    <div>
+        <h1>오늘 내용 복습하기</h1>
+        <hr />
+
+        <h2>GitHub 순서</h2>
+        <li>git add 파일명을 작성 </li>
+        <li>git commit -m "커밋 메세지" </li>
+        <li>git push origin projectJun </li>
+
+    </div>
+  )
+}
+
+export default review

8d4ac5e3f17e2d7e555f8d5ad40eea2f73dacf2d
seo <seoj224@gmail.com>
2026-03-05 16:15:26 +0900

    리액트

---
+++ TestProject/React.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useState } from 'react'
+
+const react = () => {
+
+    const [num,setNum] = useState()
+
+  return (
+    <div>react</div>
+  )
+}
+
+export default react

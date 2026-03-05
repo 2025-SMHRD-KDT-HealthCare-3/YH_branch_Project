8b8b1ebb1a6c92210bbfb4bbaece13cffa15550a
Junha <vuswnsgk724@gamil.com>
2026-03-05 16:33:09 +0900

    수정

--- TestProject/review.jsx
+++ TestProject/review.jsx
@@ -1,18 +1,24 @@
 import React from 'react'
 
-const review = () => {
+const Review = () => { // 대문자로 변경
   return (
     <div>
         <h1>오늘 내용 복습하기</h1>
         <hr />
 
         <h2>GitHub 순서</h2>
-        <li>git add 파일명을 작성 </li>
-        <li>git commit -m "커밋 메세지" </li>
-        <li>git push origin projectJun </li>
+        <ol> {/* 순서가 있는 리스트로 감싸기 */}
+            <li><code>git add</code> 파일명을 작성</li>
+            <li><code>git commit -m "커밋 메세지"</code></li>
+            <li><code>git push origin projectJun</code></li>
+        </ol>
 
+        {/* 팀원이 꼭 기억해야 할 다음 단계 */}
+        <blockquote>
+            <p>💡 Push 후에는 GitHub 사이트에서 <strong>Pull Request</strong>를 생성하세요!</p>
+        </blockquote>
     </div>
   )
 }
 
-export default review
+export default Review 
